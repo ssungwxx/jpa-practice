ce8761749b19d6a5f6eea9940f867ccc2bc87f6c
KimSungwoo <sungwoo.dev@gmail.com>
2020-08-23 15:09:18 +0900

    Create member service logic

---
+++ src/main/java/com/sw/shop/service/MemberService.java
@@ -0,0 +1,41 @@
+package com.sw.shop.service;
+
+import com.sw.shop.domain.Member;
+import com.sw.shop.repository.MemberRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public long join(Member member){
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+
+        if(!findMembers.isEmpty()){
+            throw new IllegalArgumentException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    public List<Member> findAllMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId){
+        return memberRepository.findOne(memberId);
+    }
+}
