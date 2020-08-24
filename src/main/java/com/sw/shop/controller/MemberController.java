816479f311dddf6572e5eb144ef779e3ea84bf07
KimSungwoo <sungwoo.dev@gmail.com>
2020-08-24 18:11:52 +0900

    Create member view

--- src/main/java/com/sw/shop/controller/MemberController.java
+++ src/main/java/com/sw/shop/controller/MemberController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -25,7 +26,7 @@ public class MemberController {
         return "members/createMemberForm";
     }
 
-    @PostMapping("members/new")
+    @PostMapping("/members/new")
     public String create(@Valid MemberForm form, BindingResult result){
 
         if(result.hasErrors()){
@@ -41,5 +42,13 @@ public class MemberController {
         memberService.join(member);
 
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findAllMembers();
+        model.addAttribute("members", members);
+
+        return "/members/memberList";
     }
 }
